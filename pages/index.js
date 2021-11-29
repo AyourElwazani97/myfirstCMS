38f708d887f693caca788fd6ea4ee983a98bb3d2
Your Name <you@example.com>
2021-11-29 23:16:55 +0100

    trying .yml

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
 import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
-export default class Home extends Component {
-  render() {
-    let { title, date } = attributes;
-    console.log(attributes);
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <div>
-            <button>d</button>
-          </div>
-          <h1>{date}</h1>
-        </article>
-      </>
-    );
-  }
-}
+import Image from "next/image";
+const Home = () => {
+  const URL = "https://ayour.netlify.app/";
+  let { title, date, thumbnail } = attributes;
+  console.log(attributes);
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <div>
+          <button>d</button>
+        </div>
+        <h1>{date}</h1>
+        <Image src={`/${thumbnail}`} width="300" height="300" alt="" />
+      </article>
+    </>
+  );
+};
+export default Home;
