87bc6aebc27ce2f3e919de7f4e2bbefd673b8b95
your_name <your_address_email@domain.com>
2021-11-30 13:45:09 +0100

    new dataj

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,10 @@
 import Head from "next/head";
-import { attributes, react as HomeContent } from "../posts";
 import Image from "next/image";
 const Home = () => {
-  const URL = "https://ayour.netlify.app/";
-  let { title, date, thumbnail } = attributes;
-  console.log(attributes);
+
   return (
     <>
-      <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </Head>
-      <article>
-        <h1>{title}</h1>
-        <HomeContent />
-        <div>
-          <button>d</button>
-        </div>
-        <h1>{date}</h1>
-        <Image src={`/${thumbnail}`} width="300" height="300" alt="" />
-      </article>
+      <div>h</div>
     </>
   );
 };
